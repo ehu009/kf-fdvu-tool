e3c2afa57555c789ea971ee98b9df58fa9bde443
ehu009 <jenssen.eirik@gmail.com>
2023-09-14 09:58:55 +0200

    unit test for deviation filter

--- src/deviation.js
+++ src/deviation.js
@@ -21,7 +21,24 @@ let ready = new Proxy(readyTarget, {
 				return true;
 			}
 	});
-
+function filter(deviations, rentables) {
+	/*
+		finn alle relevante bygninger
+	*/
+	let propertyMap = new Map();
+	rentables.forEach((row)  => {
+			let key = row[rentableIdx['bygningsnavn']];
+			if (propertyMap.has(key) == false) {
+				propertyMap.set(key, 0);
+			}
+		});
+	/*
+		filtrer avvik etter bygninger
+	*/
+	return deviations.filter((row) => {
+			return propertyMap.has(row[deviationIdx['bygningsnavn']]);
+		});
+}
 
 
 function begin() {
@@ -75,44 +92,27 @@ function begin() {
 			
 			document.addEventListener(eventName, () => {
 					
-					
-					/*
-						finn alle relevante bygninger
-					*/
-					let propertyMap = new Map();
-					rentables.forEach((row)  => {
-							let key = row[rentableIdx['bygningsnavn']];
-							if (propertyMap.has(key) == false) {
-								propertyMap.set(key, 0);
-							}
-						});
-					
-					/*
-						filtrer avvik etter bygninger
-					*/
-					let filteredDeviations = deviations.filter((row) => {
-							return propertyMap.has(row[deviationIdx['bygningsnavn']]);
-						});
+					let dev = filter(deviations, rentables);
 					
 					/*
 						tillat nedlasting
 					*/
 					let btn = fxcd("download");
 					btn.disabled = false;
-					downloadButton(btn, filteredDeviations, "avvik - filtrert")
+					downloadButton(btn, dev, "avvik - filtrert")
 					
 					/*
 						tegn
 					*/
 					let con = fxcd("result");
 					con.innerHTML = "";
-					filteredDeviations.forEach((deviation) => {
+					dev.forEach((d) => {
 							let p = xcd("p");
 							let out = [
-									deviation[deviationIdx['avviksnavn']],
-									deviation[deviationIdx['bygningsnavn']],
-									deviation[deviationIdx['fasilitet']],
-									deviation[deviationIdx['avviksmerknad']]
+									d[deviationIdx['avviksnavn']],
+									d[deviationIdx['bygningsnavn']],
+									d[deviationIdx['fasilitet']],
+									d[deviationIdx['avviksmerknad']]
 								];
 							axcd(p, txcd(out[0]));
 							addLine(p);
@@ -126,4 +126,34 @@ function begin() {
 					
 				});	
 		};
+}
+
+function unitTest() {
+	
+	let f = filter(deviationSample, rentableSample);
+	
+	let wanted = [
+			["Navn", "Merknad", "Prioritet", "Tilstandsgrad", "Konsekvensgrad", "Kostnad", "Ansvarlig", "Meldt av", "Fasilitet", "Eiendom", "Bygning", "Opprettet dato", "Registrert", "Utbedret", "Epost sendt", "Fra helpdesk", "Fra kontrollplan", "Fag", "Frekvens", "NS 3451", "NS 3454", "Oppgave", "Planlagt start oppgave", "Budsjett oppgave", "Avvikstype"],
+			["Montere stikk til ventilator,nytt kjøkken i u.et.", "Ventilator er montert.", "4", "1 - svake symptomer", "1 - små konsekvenser", "", "Geir Frode Olsen", "Finn Løkvoll", "118007 Åsgård Sørslettvegen 3", "1180 Åsgård", "118007 Åsgård Sørslettvegen 3", "25.11.2021", "25.11.2021", "21.01.2022", "", "False", "False", "Elektriske anlegg", "", "2 Bygning", "3 DRIFTSKOSTNADER ", "", "", "0", "Elektriske Anlegg"],
+			["Montere vaskbar plate.", "Det må monteres vaskbar plate bak komfyr, av hygieniske grunner.", "3", "1 - svake symptomer", "1 - små konsekvenser", "", "Finn Løkvoll", "Finn Løkvoll", "118007 Åsgård Sørslettvegen 3", "1180 Åsgård", "118007 Åsgård Sørslettvegen 3", "24.03.2020", "24.03.2020", "14.05.2021", "", "False", "False", "Bygg Vedlikehold", "", "2 Bygning", "3 DRIFTSKOSTNADER ", "", "", "0", "Bygningsmessig"],
+			["Montering av nye utelys.", "Det er behov for lys ute på nordsiden av bygget. Det blir gangvei for beboer i leilighet i underetasjen på denne siden av bygget. Skift ut de to utelysene som stå på østsiden av bygget + et nytt utelys på nordsiden.", "4", "1 - svake symptomer", "2 - middels store konsekvenser", "", "Geir Frode Olsen", "Finn Løkvoll", "118007 Åsgård Sørslettvegen 3", "1180 Åsgård", "118007 Åsgård Sørslettvegen 3", "15.12.2021", "15.12.2021", "", "", "False", "False", "Elektriske anlegg", "", "2 Bygning", "3 DRIFTSKOSTNADER ", "", "", "0", "Elektriske Anlegg"],
+		];
+	
+	let err = false;
+	let k = 0;
+	for (let i = 0; i < f.length; i+= 1) {
+		for (let c = 0; c < f[i].length; c += 1) {
+			let current = f[i][c];
+			let want = wanted[i][c];
+			if (current != want) {
+				err = true;
+				xc(current, want);
+				break;
+			}
+		}
+		if (err) {
+			break;
+		}
+	}
+	return err;
 }
