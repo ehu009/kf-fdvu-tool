6dddede066a5462dc9adbd7a196200f082724479
ehu009 <jenssen.eirik@gmail.com>
2023-10-10 09:59:24 +0200

    mock unit tests

--- src/kobo.js
+++ src/kobo.js
@@ -3,8 +3,43 @@ function begin() {
 
 }
 
-
 function unitTest() {
+	
+	let q = false;
+	
+	
+	return true;
+}
+
+function testEstateConnectivity() {
+	const rentable;
+	const estates;
+	
+	return true;
+}
+
+function testContractConnectivity() {
+	const rentable;
+	const contract;
+	
+	return true;
+}
+
+function testFacilityConnectivity() {
+	const rentable;
+	const facilities;
+	
+	return true;
+}
+
+function testAddressParse() {
+	const facilities;
+	
+	return true;
+}
+
+function testRoomNumberParse() {
+	const facility;
 	
 	return true;
 }
