9458e14466a5f552bc758545a64e4fe2d62198fe
ehu009 <jenssen.eirik@gmail.com>
2023-10-12 09:50:18 +0200

    no string substitution required

--- ÆØÅæøå.rb
+++ ÆØÅæøå.rb
@@ -4,18 +4,10 @@
 ARGV.each do |file_name|
 
   puts file_name
-  
   text = File.read(file_name)
-  .gsub(/Ã†/, 'Æ')
-  .gsub(/Ã˜/, 'Ø')
-  .gsub(/Ã…/, 'Å')
-  .gsub(/Ã¦/, 'æ')
-  .gsub(/Ã¸/, 'ø')
-  .gsub(/Ã¥/, 'å')
-  .encode("iso-8859-1").force_encoding("utf-8")
   
   File.open(file_name, "w") do |file|
-    file.puts text
+    file.puts text.encode("iso-8859-1").force_encoding("utf-8")
   end
   
 end
