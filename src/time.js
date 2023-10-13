846a3da4aee7e7ea4eae15e570945dc3c8579797
ehu009 <jenssen.eirik@gmail.com>
2023-10-13 12:43:14 +0200

    proper date strings

--- src/time.js
+++ src/time.js
@@ -9,7 +9,6 @@ function begin() {
 	
 }
 
-
 function unitTest() {
 	return testExact() || testBefore() || testAfter() || testBetween() || testOutside();
 }
@@ -22,7 +21,7 @@ function testExact() {
 			["2023014", "Husleie mai 2023", "7037141563", "Arnt Barnt", "244898", "K00006431", "Sørslettveien 10, H 0202", "", "Husleie", "16300", "7200979", "", "", 	"01.05.2023", "31.05.2023", "1", "9636,86", "9636,86", "9636,86", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 			["2023014", "Husleie mai 2023", "11118047356", "Martin Kattepus", "244909", "K00006444", "Lars Eriksens vei 20, U 0102", "", "Husleie", "16300", "7200979", "", "", 	"01.05.2023", "31.05.2023", "1", "9650,71", "9650,71", "9650,71", "", "01.01.2023", "01.01.2024", "False", "False", "False"]
 		];
-	let date = "01.05.2023";
+	let date = "2023-05-01";
 	
 	return arrayCompare(wanted, filter(invoiceSample, invoiceIdx['fra dato'], invoiceIdx['til dato'], "eksakt", date, null));
 }
@@ -37,7 +36,7 @@ function testBefore() {
 			["2023001", "Husleie januar 2023", "7037141563", "Arnt Barnt", "244898", "K00006431", "Sørslettveien 10, H 0202", "", "Husleie", "16300", "7200979", "", "", "01.01.2023", "31.01.2023", "1", "9636,86", "9636,86", "9636,86", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 			["2023001", "Husleie januar 2023", "21016729768", "Kjell Trell Trafikkuhell", "236676", "K00006433", "Sørslettveien 8, U 0101", "", "Husleie", "16300", "7200979", "", "", 		"01.02.2023", "28.02.2023", "1", "9739,12", "9739,12", "9739,12", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 		];
-	let date = "01.04.2023";
+	let date = "2023-04-01";
 	
 	return arrayCompare(wanted, filter(invoiceSample, invoiceIdx['fra dato'], invoiceIdx['til dato'], "før", date, null));
 }
@@ -52,7 +51,7 @@ function testAfter() {
 			["2023014", "Husleie mai 2023", "7037141563", "Arnt Barnt", "244898", "K00006431", "Sørslettveien 10, H 0202", "", "Husleie", "16300", "7200979", "", "", 	"01.05.2023", "31.05.2023", "1", "9636,86", "9636,86", "9636,86", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 			["2023014", "Husleie mai 2023", "11118047356", "Martin Kattepus", "244909", "K00006444", "Lars Eriksens vei 20, U 0102", "", "Husleie", "16300", "7200979", "", "", 	"01.05.2023", "31.05.2023", "1", "9650,71", "9650,71", "9650,71", "", "01.01.2023", "01.01.2024", "False", "False", "False"]
 		];
-	let date = "01.04.2023";
+	let date = "2023-04-01";
 	
 	return arrayCompare(wanted, filter(invoiceSample, invoiceIdx['fra dato'], invoiceIdx['til dato'], "etter", date, null));
 }
@@ -65,8 +64,8 @@ function testBetween() {
 			["2023008", "Husleie mars 2023", "26106847818", "Arne Bjarne Tjarne", "223299", "K00006421", "Nordslettveien 14, H 0102", "", "Husleie", "16300", "7200979", "", "", 	"01.03.2023", "31.03.2023", "1", "9804,1", "9804,1", "9804,1", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 			["2023001", "Husleie januar 2023", "21016729768", "Kjell Trell Trafikkuhell", "236676", "K00006433", "Sørslettveien 8, U 0101", "", "Husleie", "16300", "7200979", "", "", 		"01.02.2023", "28.02.2023", "1", "9739,12", "9739,12", "9739,12", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 		];
-	let begin = "01.02.2023";
-	let end = "15.03.2023";
+	let begin = "2023-02-01";
+	let end = "2023-03-15";
 	
 	return arrayCompare(wanted, filter(invoiceSample, invoiceIdx['fra dato'], invoiceIdx['til dato'], "mellom", begin, end));
 }
@@ -82,8 +81,8 @@ function testOutside() {
 			["2023001", "Husleie januar 2023", "7037141563", "Arnt Barnt", "244898", "K00006431", "Sørslettveien 10, H 0202", "", "Husleie", "16300", "7200979", "", "", 	"01.01.2023", "31.01.2023", "1", "9636,86", "9636,86", "9636,86", "", "01.01.2023", "01.01.2024", "False", "False", "False"],
 			["2023014", "Husleie mai 2023", "11118047356", "Martin Kattepus", "244909", "K00006444", "Lars Eriksens vei 20, U 0102", "", "Husleie", "16300", "7200979", "", "", 		"01.05.2023", "31.05.2023", "1", "9650,71", "9650,71", "9650,71", "", "01.01.2023", "01.01.2024", "False", "False", "False"]
 		];
-	let begin = "01.02.2023";
-	let end = "15.03.2023";
+	let begin = "2023-02-01";
+	let end = "2023-03-15";
 	
 	return arrayCompare(wanted, filter(invoiceSample, invoiceIdx['fra dato'], invoiceIdx['til dato'], "utenfor", begin, end));
 }
