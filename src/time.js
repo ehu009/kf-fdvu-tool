55f918586856340155abd553953d99afd0ae90b7
ehu009 <jenssen.eirik@gmail.com>
2023-10-13 11:21:07 +0200

    dummy functions and unit tests

--- src/time.js
+++ src/time.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function begin() {
+	
+}
+
+
+function unitTest() {
+	return true;
+}
+
+function testBefore() {
+	return true;
+}
+
+function testAfter() {
+	return true;
+}
+
+function testBetween() {
+	return true;
+}
+
+function testOutside() {
+	return true;
+}
