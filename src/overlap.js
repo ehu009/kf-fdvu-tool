5b32139ae765d7b3a06cc6fe0d956568f2dded4a
ehu009 <jenssen.eirik@gmail.com>
2023-10-19 09:31:08 +0200

    samples for unit tests

--- src/overlap.js
+++ src/overlap.js
@@ -61,6 +61,8 @@ function setupCustomerOverlapFilter() {
 			let table = fxcd("table");
 			table.innerHTML = "";
 			
+			
+			
 			/*
 				map seksjonsnummer -> kontrakter
 			*/
@@ -392,26 +394,58 @@ function unitTest() {
 }
 
 function customerOverlapTest() {
-	
-	//	Gategata 8 har to unike adresser
+	let sample = [
+			["Løpenummer", "Overskrift", "Ekstern ID", "Leietaker", "Nummer", "Reskontronr", "Saksbehandler", "Fra", "Til", "Sum", "Kontrakt utgår", "Regulering", "Gjengs regulering", "Fasilitetsnummer", "Fasilitet", "Eiendomsnr", "Eiendomsnavn", "Byggnr", "Byggnavn", "Kontrakttype", "Fakturatype", "Mengde", "Faktura fra", "Fakturareferanse", "E-handel faktura", "Behandlingsstatus", "Sikkerhetstype", "Sikkerhetsbeløp", "Prisperiode", "Faktureringstermin", "Terminstart", "MVA-pliktig", "Merknad", "Seksjonstype"],
+			["K00006431", "Kontrakt for Arnt Barnt", "", "Arnt Barnt", "7037141563", "244898", "", "01.01.2008", "", "9636,86", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Sørslettveien 10 H 0202", "1180", "Åsgård", "118005", "Åsgård Sørslettveien 10", "", "Etterskudd Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006432", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2008", "01.01.2013", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Fjørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Fjørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006433", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2012", "", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Fjørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Fjørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006438", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2016", "01.01.2020", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620028", "24979620028 Sørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Sørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006442", "Kontrakt for Kine Trampoline", "", "Kine Trampoline", "1118047547", "239119", "", "01.01.2008", "", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620017", "24979620017 Lars Eriksens vei 20 H 0102", "1180", "Åsgård", "118002", "Åsgård Lars Eriksens vei 20", "", "Etterskudd Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"]
+		];
+	//	Gategata 8 har flere kontrakter samtidig
 	
 	return true;
 }
 function rentableOverlapTest() {
-	
+	let sample = [
+			["Nummer", "Navn", "Merknad", "Adresse", "Fasilitet", "Eiendom", "Bygning", "Region", "Kategori bolig", "Formål", "Status utleie", "Plassering", "Standard", "Korreksjonsfaktorer", "Seksjonstype", "Mengde", "Sum", "Anskaffelsespris", "Anskaffet dato", "SSB nummer", "Antall rom", "Aktiv", "Utleibar", "Løpenummer", "Leietaker nummer", "Leietaker", "Eierform"],
+			["24100610114", "Sørslettvegen 3 - Underetasje", "Leilighet i Underetasje med egen inngang på nedsiden av huset. Består av Gang, Vaskerom, Bad, Wc i eget rom ved kjøkken/stue, Kjøkken/stue og soverom. samt en gang mellom rommene.  Brannsentral for hele huset er i hovedinngang i H0101. Sikringsskap for hele huset inklusive underetasje er i mellomgang i H0101. Stoppekran Vann: ? Overrislingsanlegg - 'sprinkling' montert ok? Brannslukkere montert i boligen?  Andre etasje er stengt av med en vegg med låst dør i. Trappedør må Tjenesten få nøkkel til for å kunne nå til sikringsskap og Brannsentral! Søppel samlekontainer på øvre side av huset. Postkasse på vegg ved hovedinngang til huset.  Boligen benyttes for spesielt behov Psykiatri  Utleiepris er basert på areal for leiligheten. Strøm som er inkludert i husleien er basert på redusert areal pga. stor gang og vaskerom. Her er strøm ut fra 50 M2 ", "Sørslettvegen 3  U0101  9016 TROMSØ ", "118007 Åsgård Sørslettvegen 3", "1180 Åsgård", "118007 Åsgård Sørslettvegen 3", "", "Bolig", "", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Normal", "Billigere (Upraktisk løsning ...)", "Rus og Psykiatribolig", "64,00", "11247,00", "", "24.01.2020", "", "2", "True", "True", "K00016564", "14116645069", "Jan Pølse Skinkerud", "Leid"],
+			["24100610115", "Sørslettvegen 3, H0101", "Hovedleilighet skal ikke tas i bruk i 2021  Boligen har en frittstående Garasje på nordsiden av huset Brannsentral for hele huset er i hovedinngang i H0101. Sikringsskap for hele huset inklusive underetasje er i mellomgang i H0101. Stoppekran Vannforsyning ?", "Sørslettvegen 3  Hooved etasje  H0101  9016 TROMSØ ", "118007 Åsgård Sørslettvegen 3", "1180 Åsgård", "118007 Åsgård Sørslettvegen 3", "", "Bolig", "", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Lav", "Ingen korreksjon", "Rus og Psykiatribolig", "90,00", "8723,20", "", "24.12.2019", "", "2", "True", "True", "K00016565", "K00012595", "Passiv", "Leid"],
+			["24979620028", "Sørslettveien 8 U 0101", "", "Sørslettveien 8  U 0101  9016 TROMSØ ", "118006 Åsgård Sørslettveien 8", "1180 Åsgård", "118006 Åsgård Sørslettveien 8", "", "Bolig", "", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Normal", "Ingen korreksjon", "Rus og Psykiatribolig", "58,20", "9691,00", "", "", "", "2", "True", "True", "K00006433", "21016729768", "Kjell Trell Trafikkuhell", "Leid - Driftet"],
+			["24979620030", "Sørslettveien 8 H 0201", "", "Sørslettveien 8  H 0201  9016 TROMSØ ", "118006 Åsgård Sørslettveien 8", "1180 Åsgård", "118006 Åsgård Sørslettveien 8", "", "Bolig", "", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Normal", "Ingen korreksjon", "Rus og Psykiatribolig", "58,30", "9696,00", "", "", "", "2", "True", "True", "K00006331", "07106638432", "Fredrik Puddingsen", "Leid - Driftet"],
+			["24979620031", "Sørslettveien 8 H 0202", "", "Sørslettveien 8  H 0202  9016 TROMSØ ", "118006 Åsgård Sørslettveien 8", "1180 Åsgård", "118006 Åsgård Sørslettveien 8", "", "Bolig", "", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Normal", "Ingen korreksjon", "Rus og Psykiatribolig", "58,30", "9696,00", "", "", "", "2", "True", "True", "K00007545", "31125545206", "Kari Kanon", "Leid - Driftet"],
+			["24979620028", "Lars Eriksens veg 17, H0201", "Leilighetsnr. 56. Felles trapperom. Brannslange, i gang utenfor boenhet. Vaskemaskin montert på kjøkken. Utvendig postkasse merket med leilighetsnr. Stor bod i kjeller merket med leilighetsnr. Kodelås på ytterdør og egen nøkkel til leilighet. Mulighet for parkering gjennom Tromsø Parkering.", "Lars Eriksens veg 17, H0201  9016 TROMSØ ", "117702 Lars Eriksens veg 17", "1177 Lars Eriksens veg", "117702 Lars Eriksens veg 17", "Tromsøya sør", "Bolig", "Ukrainabolig", "", "Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)", "Normal", "Ingen korreksjon", "Flyktningebolig", "32,00", "9900,00", "", "01.11.2022", "", "2", "True", "True", "K00017044", "08030383509", "Julis Kaviartuben", "Leid", ]
+		];
 	//	seksjonnummer 42069 har to addresser
 	
 	return true;
 }
+
+
 function contractOverlapTest() {
-	
-	//	bjørnar har to kontrakter samtidig
+	let sample = [
+			["Løpenummer", "Overskrift", "Ekstern ID", "Leietaker", "Nummer", "Reskontronr", "Saksbehandler", "Fra", "Til", "Sum", "Kontrakt utgår", "Regulering", "Gjengs regulering", "Fasilitetsnummer", "Fasilitet", "Eiendomsnr", "Eiendomsnavn", "Byggnr", "Byggnavn", "Kontrakttype", "Fakturatype", "Mengde", "Faktura fra", "Fakturareferanse", "E-handel faktura", "Behandlingsstatus", "Sikkerhetstype", "Sikkerhetsbeløp", "Prisperiode", "Faktureringstermin", "Terminstart", "MVA-pliktig", "Merknad", "Seksjonstype"],
+			["K00006431", "Kontrakt for Arnt Barnt", "", "Arnt Barnt", "7037141563", "244898", "", "01.01.2008", "", "9636,86", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Sørslettveien 10 H 0202", "1180", "Åsgård", "118005", "Åsgård Sørslettveien 10", "", "Etterskudd Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006432", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2008", "01.01.2012", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Fjørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Fjørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006433", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2013", "", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620037", "24979620037 Fjørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Fjørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006438", "Kontrakt for Kjell Trell Trafikkuhell", "", "Kjell Trell Trafikkuhell", "21016729768", "236676", "", "01.01.2016", "01.01.2020", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620028", "24979620028 Sørslettveien 8 U 0101", "1180", "Åsgård", "118006", "Åsgård Sørslettveien 8", "", "Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"],
+			["K00006442", "Kontrakt for Kine Trampoline", "", "Kine Trampoline", "1118047547", "239119", "", "01.01.2008", "", "9739,12", "01.01.3000", "01.01.2024", "01.01.2025", "24979620017", "24979620017 Lars Eriksens vei 20 H 0102", "1180", "Åsgård", "118002", "Åsgård Lars Eriksens vei 20", "", "Etterskudd Agresso", "1", "", "Husleie indeksreguleres et år etter kontrakten starter, deretter årlig.", "", "Løpende", "", "0", "Månedlig", "Månedlig", "Januar", "False", "", "Rus og Psykiatribolig"]
+		];
+	//	bjørnar har flere kontrakter samtidig
 	
 	return true;
 }
 function keyOverlapTest() {
-	
-	//	nøkkelhank nummer 113 tilhører flere seksjoner
+	let sample = [
+			["Nummer", "Navn", "Systemnr", "Antall nøkler", "Merknad", "Eiendomsnr", "Eiendomsnavn", "Bygningsnr", "Bygningsnavn", "Seksjonsnr", "Seksjonsnavn"],			
+			["546", "Gategata 8 - Mellomdør til Underetasjen", "", "2", "SKAL IKKE UTLEVERES LEIETAKER", "1180", "Åsgård", "118007", "Gategata 8", "24100610135", "Sørslettvegen 3, H0101"],
+			["546", "Sørslettvegen 3 - H0101 Reservenøkler ", "", "4", "Nøkler til hybel ved bad hovedetasjen Skal ikke utleveres", "1180", "Åsgård", "118007", "Åsgård Sørslettvegen 3", "24100610115", "Sørslettvegen 3, H0101"],
+			["546", "Sørslettvegen 3 - H0101 Ytterdør", "", "4", "Hovedinngang ", "1180", "Åsgård", "118007", "Åsgård Sørslettvegen 3", "24100610115", "Sørslettvegen 3, H0101"],
+			["547", "Vågvegn 21B - Leilighet", "Trioving nye ", "6", "5 i skap 18.04.23 EJH. Oppdater antall ved innlevering", "3094", "Solåsen Boligsameie - Org. 980379558", "309401", "Vågvegen 21 B", "14100616354", "Vågvegen 21 B"],
+			["548", "Post - Vestregata 15, Leilighet 442, H0408", "", "4", "2 i skap 18.04.23 EJH. Oppdater antall ved innlevering", "1073", "Vestregata Heracleum", "107302", "Vestregata 9/15 Heracleum", "114611217", "Vestregata 15, Leilighet 442, H0408"],
+			["548", "Vestregata 15, Leilighet 442, H0408", "XL72011 K75", "6", "3 i skap 18.04.23 EJH. Oppdater antall ved innlevering", "1073", "Vestregata Heracleum", "107302", "Vestregata 9/15 Heracleum", "114611217", "Vestregata 15, Leilighet 442, H0408"]
+		];
+	//	nøkkelhank nummer 546 tilhører flere seksjoner
 	
 	return true;
 }
