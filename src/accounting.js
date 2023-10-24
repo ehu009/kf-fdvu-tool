901d9e1e7bee97f9899da174168161a96d994835
ehu009 <jenssen.eirik@gmail.com>
2023-10-24 10:48:18 +0200

    loss calc tool works as intended

--- src/accounting.js
+++ src/accounting.js
@@ -30,7 +30,7 @@ function calcLoss(begin, end, contracts, rentables) {
 					return false;
 				}
 				let start = dateWithDefault(row[contractIdx['startdato']], defaultBegin);
-				let stop = dateWithDefault(row[contractIdx['sluttdato']], defaultBegin);
+				let stop = dateWithDefault(row[contractIdx['sluttdato']], defaultEnd);
 				
 				return temporalOverlap(begin, end, start, stop);
 			});
@@ -69,6 +69,7 @@ function calcLoss(begin, end, contracts, rentables) {
 			if (isNaN(acqPrice)) {
 				diff = 0;
 			}
+			
 			if (m.has(key)) {
 				for (let row of m.get(key)) {
 					
@@ -146,10 +147,11 @@ function beginLoss() {
 			set: (target, key, value) => {
 					target[key] = value;
 					fxcd("download").disabled = true;
-					if (target["countA"] < 1 && target["countB"] < 1 && target["dateA"] == 0 && target["dateB"] == 0) {
+					
+					if (target["countA"] < 1 && target["countB"] < 1) {
 						document.dispatchEvent(readyEvent);
 					} else {
-						if (target["countA"] < 1 && target["dateA"] < 1 && target["dateB"] < 1) {
+						if (target["countA"] < 2 && target["countB"] < 2 && target["dateA"] < 1 && target["dateB"] < 1) {
 							fxcd("filter").disabled = false;
 						} else {
 							fxcd("filter").disabled = true;
@@ -165,17 +167,17 @@ function beginLoss() {
 		
 		fxcd("rentables").onchange = (evt) => {
 				if (evt.target.files.length < 1) {
-					dataReady["fileA"] += 1;
+					ready["fileA"] += 1;
 				} else {
-					dataReady["fileA"] -= 1;
+					ready["fileA"] -= 1;
 				}
 			};
 			
 		fxcd("contracts").onchange = (evt) => {
 				if (evt.target.files.length < 1) {
-					dataReady["fileB"] += 1;
+					ready["fileB"] += 1;
 				} else {
-					dataReady["fileB"] -= 1;
+					ready["fileB"] -= 1;
 				}
 			};
 			
@@ -196,14 +198,14 @@ function beginLoss() {
 	
 	
 	
-	fxcd("date-to").onchange = (evt) => {
+	fxcd("end").onchange = (evt) => {
 			if (isInvalid(evt.target.value)) {
 				ready["dateB"] = 1;
 			} else {
 				ready["dateB"] = 0;
 			}
 		};
-	fxcd("date-from").onchange = (evt) => {
+	fxcd("begin").onchange = (evt) => {
 			if (isInvalid(evt.target.value)) {
 				ready["dateA"] = 1;
 			} else {
@@ -219,208 +221,21 @@ function beginLoss() {
 		};
 	contracts.onchange = (evt) => {
 			if (evt.target.files.length > 0) {
-				ready["countA"] -= 1;
+				ready["countB"] -= 1;
 			} else {
-				ready["countA"] += 1;
+				ready["countB"] += 1;
 			}
 		};
 	
 	
 	document.addEventListener(eventName, () => {
 			
-			let from = fxcd("date-from");
-			let to = fxcd("date-to");
-			
-			contractList.shift();
-			activeList.shift();
-			
-			// lag map s.a. [(fasilitet + nummer) -> kontraktinfo]
-			let mep = mapContracts(contractList, 4, 5);
-			
-			// legg til seksjonspris
-			for (let e of activeList) {
-				/*
-nummer, navn, sum, aktiv
-*/
-				const number = e[rentableIdx['seksjonsnummer']];
-				const name = e[rentableIdx['seksjonsnavn']];
-				if (isInvalid(name) && isInvalid(number)) {
-					continue;
-				}
-				const id = [number+" "+name, number];
-				
-				if (mep.has(id) == false) {
-					let filler = new Array(5);
-					filler[4] = e[rentableIdx['seksjonspris']];
-					if (isInvalid(number)) {
-						xc(e);
-					}
-					mep.set(id, [id.concat(filler)]);
-					
-				} else {
-					if (e[rentableIdx['aktiv']] == "False" || e[rentableIdx['utleibar']] == "False") {
-						mep.delete(id);
-						continue;
-					}
-					
-					for (let u of mep.get(id)) {
-						if (Array.isArray(u)) {
-							u.push(e[rentableIdx['seksjonspris']]);
-						} else {
-							xc(u, mep.get(id));
-						}
-					}
-				}
-			}
-			
-			
-			let calced = [];
-			{
-				const begin = new Date(fxcd("date-from").value);
-				const end = new Date(fxcd("date-to").value);
-				
-				let defaultBegin = new Date();
-				let defaultEnd = new Date();
-				defaultBegin.setFullYear(1950);
-				defaultEnd.setFullYear(2090);
-				
-				const daysTotal = millisecondsToDays(end - begin);
-				
-				for (let entry of mep.entries()) {
-					let vacant = daysTotal;
-					let vacantLoss = 0;
-					let repair = 0;
-					let repairLoss = 0;
-					
-					let current = begin;
-					let stop = end;
-					
-					
-					while (current < stop) {
-						let next = new Date(current);
-						next.setMonth(next.getMonth() + 1);
-						next.setDate(1);
-						
-						let limit = next;
-						if (next > stop) {
-							limit = stop;
-						}
-						if (isInvalid(entry[1][0][7]) == false) {
-							vacantLoss += (millisecondsToDays(limit - current) * stringToNumber(entry[1][0][7])) / numberOfDaysInMonth(current);
-						}
-						current = new Date(limit);
-					}
-					
-					
-					// lag sum
-					{
-						for (let row of entry[1]) {
-							let rep = false;
-							if (ignoreContracts.includes(row[0]) == true) {
-								rep = true;
-							}
-							
-							if (row[0] == "Passiv") {
-								continue;
-							}
-							
-							const from = dateWithDefault(row[1], defaultBegin);
-							const to = dateWithDefault(row[2], defaultEnd);
-							if (from > end || to < begin) {
-								continue;
-							}
-							
-							let cPrice = 0;
-							let sPrice = 0;
-							{
-								if (isInvalid(row[3]) == false) {
-									cPrice = stringToNumber(row[3]);
-								}
-								if (isInvalid(row[7]) == false) {
-									cPrice = stringToNumber(row[7]);
-								}
-							}
-							
-							{
-								let beginDate = begin;
-								if (from > begin) {
-									beginDate = from;
-								}
-								current = new Date(beginDate);
-								
-								let endDate = end;
-								if (to < end) {
-									endDate = to;
-								}
-								stop = new Date(endDate);
-							}
-							
-							while (current < stop) {
-								
-								let next = new Date(current);
-								next.setMonth(next.getMonth() + 1);
-								next.setDate(1);
-								
-								let limit = next;
-								if (next >= stop) {
-									limit = stop;
-								}
-								const monthDays = numberOfDaysInMonth(current);
-								const rentDays = millisecondsToDays(limit - current);
-								
-								const dailySection = sPrice / monthDays;
-								const dailyContract = cPrice / monthDays;
-								
-								
-								if (cPrice != 0) {
-									vacantLoss -= rentDays * dailyContract;
-								} else {
-									vacantLoss -= rentDays * dailySection;
-								}
-								
-								vacant -= rentDays;
-								if (rep == true) {
-									repair += rentDays;
-									if (cPrice != 0) {
-										repairLoss += rentDays * dailyContract;
-									} else {
-										repairLoss += rentDays * dailySection;
-									}
-								}
-								
-								current = new Date(limit);
-							}
-						}
-					}
-					
-					calced.push([entry[0][1], entry[0][0], vacant, numToFDVUNum(vacantLoss), repair, numToFDVUNum(repairLoss)]);
-				}
-			}
-			
-			calced.unshift(["Fasilitetsnummer", "Fasilitet", "Dager vakant", "Tap pga vakanse", "Dager vedlikehold", "Tap pga vedlikehold"]);
-			
-			let sum = [["Sum vakansetap", "Sum vedlikeholdstap", "Total"]];
-			{
-				let aggregate = [0, 0, 0];
-				const lines = arrayColFilter(calced, ["Tap pga vakanse", "Tap pga vedlikehold"]);
-				for (let i = 1; i < lines.length; i+= 1) {
-					const vacant = stringToNumber(lines[i][0]);
-					const repair = stringToNumber(lines[i][1]);
-					arrayAddition([vacant, repair, vacant + repair], aggregate);
-				}
-				for (let n of aggregate) {
-					n = numToFDVUNum(n);
-				}
-				sum.push(aggregate);
-			}
-			
-			// tegn
-			writeArrayToTable("table");
-			writeArrayToTable("sum-table");
+			let from = new Date(fxcd("begin").value);
+			let to = new Date(fxcd("end").value);
 			
 			let btn = fxcd("download");
 			btn.disabled = false;
-			downloadButton(btn, calced, "tap " + fxcd("date-from").value + " til " + fxcd("date-to").value);
+			downloadButton(btn, calcLoss(from, to, contractList, activeList), "tap " + fxcd("begin").value + " til " + fxcd("end").value);
 			hide(spinner);
 		});
 	
@@ -433,12 +248,11 @@ nummer, navn, sum, aktiv
 			f1.onload = () => {
 					activeList = CSVToArray(f1.result, ";");
 					CSVRemoveBlanks(activeList);
-					xc(activeList[0]);
-					ready["countB"] -= 1;
+					ready["countA"] -= 1;
 				};
 			f2.onload = () => {
-					contractList = arrayColFilter(CSVToArray(f2.result, ";"), ["Fasilitetsnummer", "Fasilitet", "Sum", "Fra", "Til", "Leietaker", "Kontrakttype"]);
-					xc(contractList[0]);
+					contractList = CSVToArray(f2.result, ";");
+					CSVRemoveBlanks(contractList);
 					ready["countB"] -= 1;
 				};
 			
