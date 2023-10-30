4baa811ebb52bf92f6b046f174be0448df18e460
ehu009 <jenssen.eirik@gmail.com>
2023-10-30 12:28:01 +0100

    force encoding only when necessary

--- src/force_encoding.rb
+++ src/force_encoding.rb
@@ -7,7 +7,12 @@ ARGV.each do |file_name|
   text = File.read(file_name)
   
   File.open(file_name, "w") do |file|
-    file.puts text.encode("iso-8859-1").force_encoding("utf-8")
+    unless text.valid_encoding? then
+      text.scrub
+    else
+      text = text.encode("iso-8859-1").force_encoding("utf-8")
+    end
+    file.puts text
   end
   
 end
