8e3b5e92d6e4407adcbacd931e22d2d308a60e1a
ehu009 <jenssen.eirik@gmail.com>
2023-11-07 10:04:07 +0100

    samples for estates unit test

--- src/kobo.js
+++ src/kobo.js
@@ -331,6 +331,33 @@ function unitTest() {
 
 function testEstateConnectivity() {
 	
+	const sampleA = [
+			['114613276','Lars Eriksens veg 17, H0101, Leil. 52','Leilighetsnr. 52 Felles trapperom. Brannslange, i gang utenfor boenhet. Vaskemaskin montert på kjøkken. Utvendig postkasse merket med leilighetsnr. Stor bod i kjeller merket med leilighetsnr. Kodelås på ytterdør og egen nøkkel til leilighet. Mulighet for parkering gjennom Tromsø Parkering. WiFi forsterker står i leiligheten. S/N: 073221000928747 MAC: 840112D610E6','Lars Eriksens veg 17, H0101  9016 TROMSØ ','117702 Lars Eriksens veg 17','1177 Lars Eriksens veg','117702 Lars Eriksens veg 17','Tromsøya sør','Bolig','Ukrainabolig','','Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)','Normal','Ingen korreksjon','Flyktningebolig','32,00','9900,00','','01.11.2022','','2','True','True','K00017123','230790231116','Kostiantyn Kryvoshlykov','Leid'],
+			['24979620011','Lars Eriksens Veg 11, H0204','Bygget har overrislingsanlegg - sprinkler TV og internett: 11.05.17. - Telenor ruter m/ kabler. - Canal Digital dekoder m/ kort og kabler. Mangler Canal Digital modem og fjernkontroll. Tjenesten bestiller.','Lars Eriksens Veg 11  H0204  9016 TROMSØ ','117701 Lars Eriksens veg 11','1177 Lars Eriksens veg','117701 Lars Eriksens veg 11','','Bolig','','','Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)','Normal','Ingen korreksjon','Rus og Psykiatribolig','31,10','9507,080','','01.01.1968','','2','True','True','K00017542','240703','Marthe Stenes Sætrum','Leid - Driftet'],
+			['20130610198','Lars Eriksens veg 2B','Nøkkelboks: kode 1004 3 soverom. Bod i kjeller. Uteområde: Leietaker benytter fritt tomt som ligger i naturlig tilknytning til boenheten. Antall parkeringsplasser: 1 utendørs oppstillingsplass utenfor boligen (i innkjørsel). Ekstra parkering kan leies hos Tromsø Parkering.  Dyrehold og røyking ikke tillatt.  Strøm er inkludert, ikke internett. Delvis møblert: komfyr med koketopp, kjøleskap, oppvaskmaskin og vaskemaskin. ','Lars Eriksens veg 2B, H0101  9016 TROMSØ ','307801 Lars Eriksens veg 2','3078 Lars Eriksens veg 1 - 3','307801 Lars Eriksens veg 2','Tromsøya sør','Bolig','','','','','Ingen korreksjon','Flyktningebolig','90,00','15803,00','22 000','01.12.2017','','4','True','True','K00017424','061176','Jan Suliman Wali Khail','Leid'],
+			['3078000','Lars Eriksens veg 1A','Nøkkelboks satt opp: kode 1001  3 soverom. Bod i kjeller. Uteområde: Leietaker benytter fritt tomt som ligger i naturlig tilknytning til boenheten. Antall parkeringsplasser: 1 utendørs oppstillingsplass utenfor boligen (i innkjørsel). Ekstra parkering kan leies hos Tromsø Parkering.  Dyrehold og røyking ikke tillatt.  Strøm er inkludert, ikke internett. Delvis møblert: komfyr med koketopp, kjøleskap, oppvaskmaskin og vaskemaskin. ','Lars Eriksens veg 1A, H0101  9016 TROMSØ ','307800 Lars Eriksens veg 1','3078 Lars Eriksens veg 1 - 3','307800 Lars Eriksens veg 1','Tromsøya sør','Bolig','Flyktningbolig  - Intro','','','Normal','Ingen korreksjon','Komm.bolig','90,00','15503,00','22 000','01.12.2017','','4','True','True','K00017467','(ikke oppgitt)','Tuyishime Mutesi','Leid'],
+			['3078001','Lars Eriksens veg 1B','3 soverom. Bod i kjeller. Uteområde: Leietaker benytter fritt tomt som ligger i naturlig tilknytning til boenheten. Antall parkeringsplasser: 1 utendørs oppstillingsplass utenfor boligen (i innkjørsel). Ekstra parkering kan leies hos Tromsø Parkering.  Dyrehold og røyking ikke tillatt.  Strøm er inkludert, ikke internett. Delvis møblert: komfyr med koketopp, kjøleskap, oppvaskmaskin og vaskemaskin. ','Lars Eriksens veg 1B, H0101  9016 TROMSØ ','307800 Lars Eriksens veg 1','3078 Lars Eriksens veg 1 - 3','307800 Lars Eriksens veg 1','Tromsøya sør','Bolig','','','','','Ingen korreksjon','Flyktningebolig','90,00','15503,00','22 000','01.12.2017','','4','True','True','K00017466','14038725476','Ana Beatriz Do Nascimento Sampaio','Leid'],
+			['3208131','Nordslettvegen 3B','Eier: Åsgårdmarka Eiendom AS Publicsak 22/17416 Strøm, TV-signal og internett må leietaker selv bestille. Bolig kan ikke benyttes som rus- og psykiatribolig','Nordslettvegen 3B  9016 TROMSØ ','320813 Nordslettvegen 3','32081 Nordslettvegen 3 - 6','320813 Nordslettvegen 3','Tromsøya sør','Bolig','','','Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)','Normal','Ingen korreksjon','Komm.bolig','51,50','9302,00','','22.11.2022','','2','True','True','K00017411','14049544937','Håvard Klo Jakobsen','Leid'],
+			['3208132','Nordslettvegen 4A','Nøkkelboks: Kode 4321 Eier: Åsgårdmarka Eiendom AS Publicsak 22/17416 Strøm, TV-signal og internett må leietaker selv bestille. Bolig kan ikke benyttes som rus- og psykiatribolig','Nordslettvegen 4A  9016 TROMSØ ','320814 Nordslettvegen 4','32081 Nordslettvegen 3 - 6','320814 Nordslettvegen 4','Tromsøya sør','Bolig','','','Tromsøya (minus Hamna), Tromsdalen (Tomasjordnes - Solligården)','Normal','Ingen korreksjon','Komm.bolig','119,00','14683,00','','22.11.2022','','4','True','True','K00017457','200992','Barzani Khalil Bozan','Leid']
+		];
+	
+	const sampleB = [
+			['Nummer','Navn','Adresse','Eiendom','Merknad','Hjemmelshaver','Matrikkelnr','Parkeringsplasser','Parkeringsareal','Plenareal','Bruksareal (BRA)','Totalt areal','Brøytet areal','Grunneiendomstype','Eierform','Kommune','Region','Kategori bolig','Formål','Status utleie'],
+			['118.1729','Sørslettvegen 3','','1180 Åsgård','','','.118.1729','','','','','','','','','','','','',''],
+			['118.341','Senjavegen 68','','3194 Senjavegen 68','','','.118.341','','','','','','','','','','','','',''],
+			['118.409','Lars Eriksens veg 11, 14, 15','','1177 Lars Eriksens veg','','','.118.409','','','','','','','','','','','','',''],
+			['118.506','Lars Eriksens veg 16, 17','','1177 Lars Eriksens veg','','','.118.506','','','','','','','','','','','','',''],
+			['118.507','Nordslettvegen 3 - 6','','32081 Nordslettvegen 3 - 6','','','.118.507','','','','','','','','','','','','',''],
+			['118.408','Lars Eriksens veg 2','','3078 Lars Eriksens veg 1 - 3','','','.118.408','','','','','','','','','','','','',''],
+			['118.1729','Lars Eriksens veg 1 og 3','','3078 Lars Eriksens veg 1 - 3','','','.118.1729','','','','','','','','','','','','','']
+		];
+	
+	const wanted = [koboHeader()];
+	for (let i = 0; i < sampleA.length; i += 1) {
+	wanted[5][koboIdx['gnrbnr']] = '118.507';
+	wanted[6][koboIdx['gnrbnr']] = '118.507';
+	
+	
 	return true;
 }
 
